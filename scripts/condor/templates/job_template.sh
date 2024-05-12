6f221a85eee0ccd1bc04f55ffb6624899be0b05d
Axel Ponten <axelpo@icecube.wisc.edu>
2024-05-12 13:50:33 +0200

    correct python env

--- scripts/condor/templates/job_template.sh
+++ scripts/condor/templates/job_template.sh
@@ -4,7 +4,7 @@ printf "Start time: "; /bin/date
 printf "Job is running on node: "; /bin/hostname
 printf "Job is running in directory: "; /bin/pwd
 
-eval $(/cvmfs/icecube.opensciencegrid.org/py3-v4.2.1/setup.sh)
+eval $(/cvmfs/icecube.opensciencegrid.org/py3-v4.3.0/setup.sh)
 
 # enter icetray and virtual environment, then run script
 <icetrayenv> << EOF
