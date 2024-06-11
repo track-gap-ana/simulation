a566c4eea2bcd6a06a456d6d97f07fca013e50d0
Axel Pontén <axel.ponten@physics.uu.se>
2024-06-11 11:36:40 -0500

    checking that all produced files are good

--- scripts/checks/check_sim_set.py
+++ scripts/checks/check_sim_set.py
@@ -2,12 +2,14 @@
 
 import argparse
 import glob
+import shutil
 import os
 from icecube import icetray, dataio
 
 # sim top folder
 parser = argparse.ArgumentParser(description='Check for corrupt files in the simulation set.')
 parser.add_argument('-f', '--folder', type=str, help='Path to the folder containing the simulations')
+parser.add_argument('--remove-bad', dest="remove_bad", action='store_true', default=False, help='Remove bad files?')
 
 args = parser.parse_args()
 
@@ -27,22 +29,42 @@ for f in filelist:
 
 # check each file
 badfiles = []
-for file in filelist:
-    print("Checking file:", file)
+print("Start checking files:")
+for i, file in enumerate(filelist):
     frame_counter = {}
     with dataio.I3File(file) as f:
         while f.more():
             try:
-                frame = f.pop()
+                frame = f.pop_frame()
             except:
-                print("Error popping frame!")
+                print("Error popping frame in file ", file)
                 print("Current frame_counter:", frame_counter)
                 print("Moving to next file!")
                 badfiles.append(file)
                 break # move to next file
-            stop = frame.Stop
+            stop = frame.Stop.id
             if stop in frame_counter:
                 frame_counter[stop] += 1
             else:    
                 frame_counter[stop] = 1
-    print(frame_counter)
+        print("Checked file ", i, " with frames:", frame_counter)
+
+# write bad files to a textfile
+print("Bad files:", badfiles)
+if args.remove_bad:
+    parentfolder = os.path.dirname(os.path.dirname(badfiles[0]))
+    print("Writing bad file list to ", parentfolder)
+    with open(parentfolder + "/bad_files_removed.txt", "w") as f:
+        for b in badfiles:
+            f.write(b + "\n")
+    # delete
+    for f in badfiles:
+        dir_path = os.path.dirname(f)
+        # Check if the directory exists before trying to delete it
+        if os.path.exists(dir_path):
+            shutil.rmtree(dir_path)
+            print(f"The directory {dir_path} has been deleted successfully.")
+        else:
+            print(f"The directory {dir_path} does not exist.")
+
+        
