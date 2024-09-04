e0afda4e4e1204beabff08c1ffcf299bb95dd8c8
Axel Ponten <axelpo@icecube.wisc.edu>
2024-09-04 08:05:51 +0200

    running common variables skeleton

--- scripts/CommonVariables/compute_all_CV.py
+++ scripts/CommonVariables/compute_all_CV.py
@@ -1,50 +1,26 @@
 import icecube
 import icecube.icetray as icetray
-from icecube import icetray, dataclasses, dataio, phys_services, hdfwriter
+from icecube import icetray, dataclasses, dataio, phys_services
 from icecube.phys_services.which_split import which_split
 from icecube.icetray import I3Tray, I3Units
 from icecube.common_variables import direct_hits, hit_multiplicity, track_characteristics, hit_statistics, time_characteristics
-import argparse
-import glob
 
-# argument parser
-parser = argparse.ArgumentParser(description='Compute common variables and save them to an HDF5 file.')
-parser.add_argument('--inputfolder', type=str, default="", help='Path to the folder with input files.')
-parser.add_argument('--input', type=str, default="", help='Path to the input file.')
-parser.add_argument('--gcdfile', type=str, help='Path to the GCD file')
-parser.add_argument('--hdf_filename', type=str, default='all_CV.hdf5', help='Name of the output HDF5 file')
-parser.add_argument('--pulses_map_name', type=str, default='CleanedInIcePulses', help='Name of the pulses map')
-parser.add_argument('--reco_particle_name', type=str, default='PoleMuonLlhFit', help='Name of the reconstructed particle')
-parser.add_argument('--subeventstream', type=str, default='InIceSplit', help='Name of the subevent stream')
-parser.add_argument('--bookit', action='store_true', help='Flag to enable booking')
+# filename
+filename = "/home/axel/i3/LLP-data/offline_processed/earlyLibra_10E3_full_offline_preprocess_08222024_earlyLeo_full_online_preprocess_08192024_LLPSimulation.DarkLeptonicScalar.mass-110.0.eps-3e-05.bias-1000000.0.nevents-50_21466947_214962476.i3.gz"
+gcdfile = "/home/axel/i3/GeoCalibDetectorStatus_2021.Run135903.T00S1.Pass2_V1b_Snow211115.i3.gz"
 
-args = parser.parse_args()
+outputname = "test_all_CV.i3.gz"
 
-# filename is a comma-separated list of files
-inputfolder = args.inputfolder
-input = args.input
-assert input != "" or inputfolder != "", "Need either input or inputfolder"
-assert not(input != "" and inputfolder != ""), "Use either input or inputfolder, not both"
-gcdfile = args.gcdfile
-hdf_filename = args.hdf_filename
-pulses_map_name = args.pulses_map_name
-reco_particle_name = args.reco_particle_name
-subeventstream = args.subeventstream
-bookit = args.bookit
-
-# get list of files
-if inputfolder != "":
-    print("Many files")
-    filename_list = glob.glob(inputfolder + '*.i3.gz')
-else:
-    print("Single file")
-    filename_list = [input]
+# settings
+pulses_map_name    = 'CleanedInIcePulses'
+reco_particle_name = 'PoleMuonLlhFit'
+subeventstream = "InIceSplit"
+bookit = False
 
 # tray
-tableio_keys_to_book = []
 tray = I3Tray()
 tray.AddModule("I3Reader", "reader",
-               FilenameList = [gcdfile] + filename_list,
+               FilenameList = [gcdfile, filename]
 )
 
 ##### DIRECT HITS #####
@@ -55,7 +31,6 @@ print('Calculating direct hits for "%s" pulses and "%s" reco particle, using '\
 for dh_def in dh_defs:
     print(dh_def)
 
-tableio_keys_to_book +=\
 tray.AddSegment(direct_hits.I3DirectHitsCalculatorSegment, 'dh',
     DirectHitsDefinitionSeries       = dh_defs,
     PulseSeriesMapName               = pulses_map_name,
@@ -67,7 +42,6 @@ tray.AddSegment(direct_hits.I3DirectHitsCalculatorSegment, 'dh',
 
 ##### HIT MULTIPLICITY #####
 print('Calculating hit multiplicity for "%s" pulses' % (pulses_map_name))
-tableio_keys_to_book +=\
 tray.AddSegment(hit_multiplicity.I3HitMultiplicityCalculatorSegment, 'hm',
     PulseSeriesMapName                = pulses_map_name,
     OutputI3HitMultiplicityValuesName = 'HitMultiplicityValues',
@@ -82,7 +56,6 @@ print('Calculating time characteristics for "%s" '\
         'pulses' %\
         (pulses_map_name))
 
-tableio_keys_to_book +=\
 tray.AddSegment(time_characteristics.I3TimeCharacteristicsCalculatorSegment, 'timec',
     PulseSeriesMapName                     = pulses_map_name,
     OutputI3TimeCharacteristicsValuesName = reco_particle_name+'TimeCharacteristics',
@@ -98,7 +71,6 @@ print('Calculating track characteristics for "%s" '\
         'radius.'%\
         (pulses_map_name, reco_particle_name, track_cylinder_radius))
 
-tableio_keys_to_book +=\
 tray.AddSegment(track_characteristics.I3TrackCharacteristicsCalculatorSegment, 'trackc',
     PulseSeriesMapName                     = pulses_map_name,
     ParticleName                           = reco_particle_name,
@@ -110,8 +82,6 @@ tray.AddSegment(track_characteristics.I3TrackCharacteristicsCalculatorSegment, '
 
 ##### HIT STATISTICS #####
 print('Calculating hit statistics for "%s" pulses' % (pulses_map_name))
-
-tableio_keys_to_book +=\
 tray.AddSegment(hit_statistics.I3HitStatisticsCalculatorSegment, 'hs',
     PulseSeriesMapName              = pulses_map_name,
     OutputI3HitStatisticsValuesName = 'HitStatisticsValues',
@@ -120,15 +90,14 @@ tray.AddSegment(hit_statistics.I3HitStatisticsCalculatorSegment, 'hs',
     If = which_split(subeventstream),
 )
 
-print("Len of tableio_keys_to_book: ", len(tableio_keys_to_book))
-print("tableio_keys_to_book: ")
-[print(key) for key in tableio_keys_to_book]
-print("Writing %s to HDF5 file: %s" % (tableio_keys_to_book, hdf_filename))
-tray.AddSegment(hdfwriter.I3HDFWriter, 'hdfwriter',
-    Keys            = tableio_keys_to_book,
-    SubEventStreams = [subeventstream],
-    Output          = hdf_filename
-)
+
+# writer, will add 8 new frame objects from the common variables
+# overwrites PoleMuonLlhFitDirectHitsBaseC which was already in frame, and its not the same!! maybe different pulse series?
+tray.Add("I3Writer", "writer", filename = outputname,
+               Streams = [icetray.I3Frame.TrayInfo, icetray.I3Frame.Simulation,
+                        icetray.I3Frame.DAQ, icetray.I3Frame.Physics],
+              DropOrphanStreams=[icetray.I3Frame.Geometry,
+            icetray.I3Frame.Calibration, icetray.I3Frame.DetectorStatus])
 
 # execute
 tray.Execute()
