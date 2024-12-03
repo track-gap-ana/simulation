ea2c35fbd7ee74fe3275d57f5638a7117c3b81c2
Axel Pontén <axel.ponten@physics.uu.se>
2024-12-03 04:28:44 -0600

    updated fix run id'

--- scripts/checks/fix_run_id_for_buggy_set.py
+++ scripts/checks/fix_run_id_for_buggy_set.py
@@ -3,55 +3,35 @@
 import icecube
 from icecube import icetray, dataio, dataclasses
 from icecube.icetray import I3Tray
+
+import os
 import glob
 
-# # top folder path
-# topfolder="/data/user/axelpo/LLP-data/DarkLeptonicScalar.mass-110.eps-3e-05.nevents-150000.0_ene_2000.0_15000.0_gap_100.0_240602.210981234/"
+parentdir = "/data/user/axelpo/LLP-data/"
+folder = "DarkLeptonicScalar.mass-110.eps-3e-05.nevents-150000.0_ene_2000.0_15000.0_gap_100.0_240602.210981234/"
 
-# # get all folder
-# filelist = glob.glob(topfolder + "LLP*/*.i3.gz")
+inputfilelist = glob.glob(parentdir+folder+"*/*.gz")
+folders = [os.path.dirname(f) for f in inputfilelist]
+filenames = [f.split("/")[-1] for f in inputfilelist]
 
-
-filelist = [
-'TESTLLPSimulation.210981234.34/LLPSimulation.DarkLeptonicScalar.mass-110.0.eps-3e-05.bias-1000000.0.nevents-500.i3.gz']
-
-
-def testfunc(frame):
-    frame["hi"] = dataclasses.I3Double(-99)
+def FixRunID(frame, procid):
+    datasetid = 776
+    if "OldI3EventHeader" in frame:
+        oldheader = frame["OldI3EventHeader"]
+        newheader = oldheader
+        newrunid = datasetid*100000 + procid
+        newheader.run_id = newrunid
+        frame["I3EventHeader"] = newheader
     return True
-tray = I3Tray()
-tray.Add("I3Reader", "reader", FilenameList=filelist)
-# tray.AddModule(testfunc, Streams = [icecube.icetray.I3Frame.DAQ])
-# tray.Add("I3Writer", "writer", filename = filelist[0])
-tray.Add("I3Writer", "writer", filename = "test.i3")
 
-tray.Execute()
-# # get procID from each file
-# def id_from_path(filename):
-#     # split to get folder with procid
-#     folder = filename.split("/")[-2]
-#     procid = folder.split('.')[-1]
-#     return int(procid)
-
-# # open all files and change I3EventHeader
-# # from condor submit: --runid $$([<datasetid>*100000+$(ProcId)]) \
-# datasetid = 776*100000
-
-# for filename in filelist:
-#     procid = id_from_path(filename)
-#     runid = datasetid + procid
-#     # open and change all frames
-#     with dataio.I3File(filename, 'w') as file:
-#         while file.more():
-#             print("hello")
-#             frame = file.pop_frame()
-#             if "I3EventHeader" in frame:
-#                 old_header = frame["I3EventHeader"]
-#                 new_header = old_header
-#                 new_header.run_id = runid
-#                 del frame["I3EventHeader"]
-#                 print(new_header)
-#                 frame["I3EventHeader"] = new_header
-#                 file.push(frame)
-
-    
+for i, (filepath, folder, name) in enumerate(zip(inputfilelist, folders, filenames)):
+    procid = int(folder.split(".")[-1])
+    print("Doing file ", name)
+    # create tray
+    tray = I3Tray()
+    tray.Add("I3Reader", "reader", Filename=filepath)
+    tray.AddModule("Rename", "rename header", Keys = ["I3EventHeader", "OldI3EventHeader"])
+    tray.Add(FixRunID, "fix id", procid=procid, Streams=[icetray.I3Frame.DAQ])
+    tray.Add("Delete", "delete old header", Keys=["OldI3EventHeader"])
+    tray.Add("I3Writer", "writer", filename=folder+"/fixedid_"+name)
+    tray.Execute()
